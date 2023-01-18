1ef3f6b5c3fe29f7c5b16fb6381ded939604eeb6
Sebastian009w <ia3266505@gmail.com>
2023-01-18 09:51:36 -0500

    Se instala la libreria de toastify

--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,73 @@
+import { v4 } from "uuid";
 import "./index.css";
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
+
 const $taskForm = document.querySelector<HTMLFormElement>("#taskForm");
 const $taskList = document.querySelector<HTMLDivElement>("#taskList");
 interface Task {
+  id: string;
   title: string;
   description: string;
 }
-let task: Array<Task> = [];
+let tasks: Array<Task> = [];
 
 $taskForm?.addEventListener("submit", (e) => {
   e.preventDefault();
   const $title = $taskForm["title"] as unknown as HTMLInputElement;
-  const $description = $taskForm["description"] as unknown as HTMLTextAreaElement;
+  const $description = $taskForm[
+    "description"
+  ] as unknown as HTMLTextAreaElement;
 
-  task.push( { title: $title.value, description: $description.value } );
-  localStorage.setItem("task", JSON.stringify(task));
-
-  renderTask(task);
+  tasks.push({
+    id: v4(),
+    title: $title.value,
+    description: $description.value,
+  });
+  localStorage.setItem("task", JSON.stringify(tasks));
+  Toastify({
+    text: "task save !",
+  }).showToast();
+  renderTask(tasks);
   $taskForm.reset();
   $title.focus();
 });
 
-window.addEventListener('DOMContentLoaded', () => {
-  task = JSON.parse(localStorage.getItem('task') || '[]');
-  renderTask(task);
-})
+window.addEventListener("DOMContentLoaded", () => {
+  tasks = JSON.parse(localStorage.getItem("task") || "[]");
+  renderTask(tasks);
+});
 
-const renderTask = (task: Array<Task>) =>  {
-  $taskList!.innerHTML = '';
-  
-  task.forEach(task => {
-    const $card = document.createElement('div');
-    $card.className = 'bg-zinc-800 mb-1 p-4 roundend-lg hover:bg-zinc-900 hover:cursor-pointer';
+const renderTask = (task: Array<Task>) => {
+  $taskList!.innerHTML = "";
 
-    const $headerTitle = document.createElement('header');
-    $headerTitle.className = 'flex justify-between items-center'
-    const $title = document.createElement('h2');
+  task.forEach((task) => {
+    const $card = document.createElement("div");
+    $card.className =
+      "bg-zinc-800 mb-1 p-4 roundend-lg hover:bg-zinc-900 hover:cursor-pointer";
+
+    const $headerTitle = document.createElement("header");
+    $headerTitle.className = "flex justify-between items-center";
+    const $title = document.createElement("h2");
     $title.textContent = task.title;
 
-    const $delete = document.createElement('button');
-    $delete.textContent = 'DELETE';
-    $delete.className = 'bg-red-500 px-2 py-1 roundend-md'
+    const $delete = document.createElement("button");
+    $delete.textContent = "DELETE";
+    $delete.className = "bg-red-500 px-2 py-1 roundend-md";
 
-    const $description = document.createElement('p');
+    $delete.addEventListener("click", () => {
+      const index = tasks.findIndex((t) => t.id === task.id);
+      tasks.splice(index, 1);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      renderTask(tasks);
+    });
+    const $description = document.createElement("p");
     $description.textContent = task.description;
-
-    
 
     $headerTitle.append($title);
     $headerTitle.append($delete);
     $card.append($headerTitle);
     $card.append($description);
     $taskList?.append($card);
-  })
-}
+  });
+};
