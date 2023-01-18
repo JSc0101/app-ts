b5a7bc26225162904124c01256b542ae0e3a9bee
Sebastian009w <ia3266505@gmail.com>
2023-01-18 09:15:35 -0500

    Se añade la tarea en la interfaz

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,39 @@
 import "./index.css";
-const taskForm = document.querySelector<HTMLFormElement>("#taskForm");
-
+const $taskForm = document.querySelector<HTMLFormElement>("#taskForm");
+const $taskList = document.querySelector<HTMLDivElement>("#taskList");
 interface Task {
   title: string;
   description: string;
 }
-const task: Task[] = [];
+let task: Array<Task> = [];
 
-taskForm?.addEventListener("submit", (e) => {
+$taskForm?.addEventListener("submit", (e) => {
   e.preventDefault();
-  const $title = taskForm["title"] as unknown as HTMLInputElement;
+  const $title = $taskForm["title"] as unknown as HTMLInputElement;
+  const $description = $taskForm["description"] as unknown as HTMLTextAreaElement;
 
-  const $description = taskForm[
-    "description"
-  ] as unknown as HTMLTextAreaElement;
-
-  task.push({
-    title: $title.value,
-    description: $description.value,
-  });
-
+  task.push( { title: $title.value, description: $description.value } );
   localStorage.setItem("task", JSON.stringify(task));
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+  task = JSON.parse(localStorage.getItem('task') || '[]');
+  renderTask(task);
+})
+
+const renderTask = (task: Array<Task>) =>  {
+  task.forEach(task => {
+    const $card = document.createElement('div');
+    const $headerTitle = document.createElement('header');
+    const $title = document.createElement('h2');
+    $title.textContent = task.title;
+
+    const $description = document.createElement('p');
+    $description.textContent = task.description;
+
+    $headerTitle.append($title);
+    $card.append($headerTitle);
+    $card.append($description);
+    $taskList?.append($card);
+  })
+}
