c66319a1abf3ac7887a4f8257ffeb71fc43c5fde
Sebastian009w <ia3266505@gmail.com>
2023-01-18 00:02:10 -0500

    Se crean tareas y se almacenan en el localstorage

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
-import './index.css';
+import "./index.css";
+const taskForm = document.querySelector<HTMLFormElement>("#taskForm");
+
+interface Task {
+  title: string;
+  description: string;
+}
+const task: Task[] = [];
+
+taskForm?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const $title = taskForm["title"] as unknown as HTMLInputElement;
+
+  const $description = taskForm[
+    "description"
+  ] as unknown as HTMLTextAreaElement;
+
+  task.push({
+    title: $title.value,
+    description: $description.value,
+  });
+
+  localStorage.setItem("task", JSON.stringify(task));
+});
