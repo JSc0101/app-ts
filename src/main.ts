ee646e2c13c986ff90182f2de7e441c80e98b6f0
Sebastian009w <ia3266505@gmail.com>
2023-01-18 09:28:35 -0500

    Se estilizan las tareas

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,10 @@ $taskForm?.addEventListener("submit", (e) => {
 
   task.push( { title: $title.value, description: $description.value } );
   localStorage.setItem("task", JSON.stringify(task));
+
+  renderTask(task);
+  $taskForm.reset();
+  $title.focus();
 });
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -22,16 +26,28 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 
 const renderTask = (task: Array<Task>) =>  {
+  $taskList!.innerHTML = '';
+  
   task.forEach(task => {
     const $card = document.createElement('div');
+    $card.className = 'bg-zinc-800 mb-1 p-4 roundend-lg hover:bg-zinc-900 hover:cursor-pointer';
+
     const $headerTitle = document.createElement('header');
+    $headerTitle.className = 'flex justify-between items-center'
     const $title = document.createElement('h2');
     $title.textContent = task.title;
+
+    const $delete = document.createElement('button');
+    $delete.textContent = 'DELETE';
+    $delete.className = 'bg-red-500 px-2 py-1 roundend-md'
 
     const $description = document.createElement('p');
     $description.textContent = task.description;
 
+    
+
     $headerTitle.append($title);
+    $headerTitle.append($delete);
     $card.append($headerTitle);
     $card.append($description);
     $taskList?.append($card);
